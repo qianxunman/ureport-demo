9f14e0b2c26064d425e65c4d3825c19b143d8fc0
jerry <jerry@hs.com>
2021-07-25 14:10:21 +0800

    udpat

--- src/main/java/cn/wilmar/ureport/report/jpa/TestBuildinDatasource.java
+++ src/main/java/cn/wilmar/ureport/report/jpa/TestBuildinDatasource.java
@@ -3,13 +3,14 @@ import java.sql.Connection;
 public class TestBuildinDatasource implements BuildinDatasource {
     private Datasource datasource;
     public String name() {
-        return "内置数据源DENO"；
-    {
+        return "内置数据源DENO";
+    }
     public Connection getConnection() {
-    try {
-        return dataSource.getConnection();
-    }catch (sQLException e) {
-        throw new RuntimeException(e);
+        try {
+            return dataSource.getConnection();
+        } catch (sQLException e) {
+            throw new RuntimeException(e);
+        }
     }
     public void setDatasource(Datasource datasource) {
         this.datasource = datasource;
